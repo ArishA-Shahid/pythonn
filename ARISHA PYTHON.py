4d594e17096727e7bed7d63e97263ea9bc5eeb93
ArishA-Shahid <156994069+ArishA-Shahid@users.noreply.github.com>
2024-01-23 15:26:44 +0500

    Add files via upload

---
+++ ARISHA PYTHON.py
@@ -0,0 +1,14 @@
+a=int(input('enter a number 1:'))
+b=int(input('enter a number 2:'))
+c=int(input('enter 1 for addition, enter 2 for subtraction, enter 3 for multiplication, enter 4 for division'))
+if(c==1):
+    print(a+b)
+elif(c==2):
+    print(a-b)
+elif(C==3):
+    print(a/b)
+elif(c==4):
+    print(a*b)
+    
+else:
+    ("invalid no")

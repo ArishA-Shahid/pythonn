7afbd39af2e964af29f3a89104f4777e2a2c16d6
ArishA-Shahid <156994069+ArishA-Shahid@users.noreply.github.com>
2024-01-30 15:45:47 +0500

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,6 @@
+#Area of Circle
+r= int (input("enter the radius of circle  :"))
+
+PI= float (3.142)
+
+print (2*PI*radius)
---
+++ average.py
@@ -0,0 +1,7 @@
+#user input
+x=int (input("enter as:  "))
+y=int (input("eneter as:   "))
+
+average=x+y/2
+    
+print ("average")
